3be83aa8e9471e8091f14a41c9b1b498ad53e9c9
Xiaoming Yang <yangxiaoming295@gmail.com>
2022-02-19 23:45:51 -0500

    added FourOFour.js & New.js files

---
+++ front-end/src/Pages/FourOFour.js
@@ -0,0 +1,9 @@
+function FourOFour() {
+  return (
+    <p>
+      Page Not Found
+    </p>
+  )
+}
+
+export default FourOFour;
---
+++ front-end/src/Pages/New.js
@@ -0,0 +1,11 @@
+import BookNewForm from "../Components/BookNewForm";
+
+function New() {
+  return (
+    <div className="New">
+      <BookNewForm />
+    </div>
+  )
+}
+
+export default New;
