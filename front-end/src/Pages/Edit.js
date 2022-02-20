1a2c6c8c4090180f3983e3022432a7be8a7d0bf1
Xiaoming Yang <yangxiaoming295@gmail.com>
2022-02-19 21:42:12 -0500

    added Edit.js file

---
+++ front-end/src/Pages/Edit.js
@@ -0,0 +1,12 @@
+import BookEditForm from "../Components/BookEditForm";
+
+function Edit() {
+  return (
+    <div className="New Edit">
+      <h2>Edit</h2>
+      <BookEditForm />
+    </div>
+  )
+}
+
+export default Edit;
