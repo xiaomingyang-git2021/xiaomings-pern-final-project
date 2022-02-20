636a2f6b83add927e5ee688d02c512dc2406077a
Xiaoming Yang <yangxiaoming295@gmail.com>
2022-02-19 20:42:11 -0500

    added a Show.js file

---
+++ front-end/src/Pages/Show.js
@@ -0,0 +1,12 @@
+import BookDetails from "../Components/BookDetails";
+
+function Show() {
+  return (
+    <div className="show">
+      <h2>Show</h2>
+      <BookDetails />
+    </div>
+  )
+}
+
+export default Show;
