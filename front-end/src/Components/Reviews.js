e4cccdc76431a4b31c0feb52a6970532cafa02ce
Xiaoming Yang <yangxiaoming295@gmail.com>
2022-02-21 23:14:20 -0500

    modified Reviews.js file

---
+++ front-end/src/Components/Reviews.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import Review from "./Review";
+import ReviewForm from "./ReviewForm";
+
+const API = process.env.REACT_APP_API_URL;
+
+function Reviews() {
+  const [reviews, setReviews] = useState([]);
+  let { id } = useParams();
+
+  useEffect(() => {
+    axios.get(`${API}/books/${id}/reviews`).then
+    ((response) => {
+      setReviews(response.data);
+    });
+  }, [id, API]);
+
+  
+}
+
+export default Reviews;
