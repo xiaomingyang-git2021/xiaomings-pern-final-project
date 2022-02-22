b3d2f530075fd6fe0ccf9b38976a352e51d2b5e2
Xiaoming Yang <yangxiaoming295@gmail.com>
2022-02-21 22:53:09 -0500

    modified ReviewForm.js file

--- front-end/src/Components/ReviewForm.js
+++ front-end/src/Components/ReviewForm.js
@@ -37,6 +37,54 @@ function ReviewForm(props) {
       book_id: id,
     });
   };
+  return (
+    <div className="Edit">
+      {props.children}
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="reviewer">Name:</label>
+        <input
+          id="reviewer"
+          value={review.reviewer}
+          type="text"
+          onChange={handleTextChange}
+          placeholder="Your name"
+          required
+        />
+        <label htmlFor="title">Title:</label>
+        <input
+          id="title"
+          type="text"
+          required
+          value={review.title}
+          onChange={handleTextChange}
+        />
+        <label htmlFor="rating">Rating:</label>
+        <input
+          id="rating"
+          type="number"
+          name="rating"
+          min="0"
+          max="5"
+          step="1"
+          value={review.rating}
+          onChange={handleTextChange}
+        />
+        <label htmlFor="content">Review:</label>
+        <textarea
+          id="content"
+          type="text"
+          name="content"
+          value={review.content}
+          placeholder="What do you think..."
+          onChange={handleTextChange}
+        />
+
+        <br />
+
+        <input type="submit" />
+      </form>
+    </div>
+  );
 };
 
 export default ReviewForm;
