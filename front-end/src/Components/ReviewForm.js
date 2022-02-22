dd2df926845d1010d1ccdec31af4fdb0fa506f40
Xiaoming Yang <yangxiaoming295@gmail.com>
2022-02-21 22:15:29 -0500

    modified ReviewForm.js file

--- front-end/src/Components/ReviewForm.js
+++ front-end/src/Components/ReviewForm.js
@@ -2,7 +2,16 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 function ReviewForm(props) {
+  let { id } = useParams();
+  const { reviewDetails } = props;
 
+  const [review, setReview] = useState({
+    reviewer: "",
+    title: "",
+    content: "",
+    rating: "",
+    book_id: id,
+  });
 };
 
 export default ReviewForm;
