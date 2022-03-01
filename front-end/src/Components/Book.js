73867cbd63b3eb4c590b238050466fb00ce6af1e
Xiaoming Yang <yangxiaoming295@gmail.com>
2022-02-28 21:03:56 -0500

    modified a file

--- front-end/src/Components/Book.js
+++ front-end/src/Components/Book.js
@@ -4,7 +4,7 @@ import ContentHealth from "./ContentHealth";
 function Book({ book }) {
   return (
     <div className="Book">
-      <img src={book.image} alt={book.name}/>
+      <img width="250px" height="320px" src={book.image} alt={book.name}/>
       <Link to={`/books/${book.id}`}>
         <h4>{book.name}</h4>
         <h4 className="book-featured"><ContentHealth bookHealth={book.featured}/></h4>
